8c3b0c91402d2ff3cd65ee545c22005ba6b884ca
Zane Oliver <zaneoliver6@gmail.com>
2015-11-24 23:59:42 -0400

    Set application key

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomString'),
+    'key' => env('APP_KEY', 'VFkfs5x8EP5sQneeNO9bcfPSM2sUSdhu'),
 
     'cipher' => 'AES-256-CBC',
 

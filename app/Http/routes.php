2a7fb6afd788cdfa6c796d8d4835e30bf28fa5f1
Zane Oliver <zaneoliver6@gmail.com>
2015-11-28 20:07:57 -0400

    Add routes for Authentication/password reset controllers

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,13 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+//Main "Home" screen
+Route::get('/','Dashboard@index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Authenication Routes
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/login', 'Auth\AuthController@getLogout');
+
+//Registration Routes
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::get('auth/register', 'Auth\AuthController@postRegister');
